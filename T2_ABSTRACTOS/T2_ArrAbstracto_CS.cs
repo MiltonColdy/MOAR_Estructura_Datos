76c19b2a77f014420270bee8276b914c6ae44d5c
MiltonColdy <miltonapodacar15@gmail.com>
2025-09-07 16:59:00 -0700

    Tipo de dato abstracto en arreglos

---
+++ T2_ABSTRACTOS/T2_ArrAbstracto_CS.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+class T2_ArrAbstracto_CS
+{
+    static void Main(string[] args)
+    {
+        Alumno[] alumnos =
+        {
+        new Alumno("Alcaraz","Pérez","Flérida","Romina",(byte)19),
+        new Alumno("Apodaca","Romero","Milton","Omar",(byte)19),
+        new Alumno("López","Medina","Ximena",nom2:"",(byte)19),
+        new Alumno("Rodríguez","Quiros","Jesús","Alfredo",(byte)19)
+        };
+    
+        for (int i = 0; i < alumnos.Length; i++)
+        {
+            if (alumnos[i].Nom2.Equals("")) //Se hace uso del Nom2 (versión pública de solo lectura)
+            {
+                alumnos[i].Datos1N();
+            }
+            else
+            {
+                alumnos[i].Datos2N();
+            }
+        }
+    }
+}
+
+class Alumno
+{
+    string ap1; string ap2;
+    string nom1; string nom2;
+    byte ed;
+
+    public Alumno(string ap1, string ap2, string nom1, string nom2, byte ed)
+    {
+        this.ap1 = ap1;
+        this.ap2 = ap2;
+        this.nom1 = nom1;
+        this.nom2 = nom2;
+        this.ed = ed;
+    }
+    
+    public void Datos2N()
+    {
+        Console.WriteLine("Nombre completo de alumn@: " + ap1 + " " + ap2 + " " + nom1 + " " + nom2);
+        Console.WriteLine("Edad de alumn@: " + ed);
+    }
+    public string Nom2 => nom2; //Esto se encarga de convertir la propiedad privada nom2 a una pública de solo lectura.
+    public void Datos1N()
+    {
+        Console.WriteLine("Nombre completo de alumn@: " + ap1 + " " + ap2 + " " + nom1);
+        Console.WriteLine("Edad de alumn@: " + ed);
+    }
+}
--- T2_ABSTRACTOS/T2_ArrAbstracto_JAVA.java
+++ T2_ABSTRACTOS/T2_ArrAbstracto_JAVA.java
@@ -1,11 +1,14 @@
 package T2_ABSTRACTOS;
+// Se define la clase abstracta "Alumno".
 class Alumno {
+    //Estas son sus propiedades.
         String ap1; String ap2;
         String nom1; String nom2;
         Byte ed;
-
+    //Aquí ordenamos sus propiedades
         public Alumno(String ap1, String ap2, String nom1, String nom2, Byte ed) 
         {
+            //Definimos la variable que le corresponderá a cada propiedad
             this.ap1 = ap1;
             this.ap2 = ap2;
             this.nom1 = nom1;
@@ -13,7 +16,7 @@ class Alumno {
             this.ed = ed;
         }
     
-        public void Datos2N() 
+        public void Datos2N() //Función para imprimir en caso de tener 2 nombres
     {
         {
         System.out.println("Nombre completo del alumno: " + ap1 + " " + ap2 + " " + nom1 + " " + nom2);
@@ -21,7 +24,7 @@ class Alumno {
         }
     }
   
-      public void Datos1N() 
+      public void Datos1N() //Función para imprimir en caso de tener 1 solo nombre
     {
         {
         System.out.println("Nombre completo del alumno: " + ap1 + " " + ap2 + " " + nom1);
@@ -29,6 +32,11 @@ class Alumno {
         }
     }
 }
+/* Cabe aclarar que en este caso, no importa realmente si son 2 nombres o 1, pues no se notará la
+   diferencia dentro de la terminal. Esto podría notarse en un programa más elaborado, así que
+   prefiero dejar la validación.
+*/ 
+
 
 public class T2_ArrAbstracto_JAVA 
 {
@@ -42,11 +50,12 @@ public class T2_ArrAbstracto_JAVA
         new Alumno("Rodríguez","Quiros","Jesús","Alfredo",(byte)19)
        };
 
-       for (byte i = 0; i < 4; i++) {
+       for (byte i = 0; i < alumnos.length; i++) {
         if (alumnos[i].nom2.equals(""))
         {
             alumnos[i].Datos1N();
-        } else
+        } 
+        else
         {
             alumnos[i].Datos2N();
         }
