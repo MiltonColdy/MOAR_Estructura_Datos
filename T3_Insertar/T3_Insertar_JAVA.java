0e1c8f9f908d76ead16985807b1d57f70bf57f90
MiltonColdy <miltonapodacar15@gmail.com>
2025-09-07 22:41:07 -0700

    Recorridos

---
+++ T3_Insertar/T3_Insertar_JAVA.java
@@ -0,0 +1,14 @@
+package T3_Insertar;
+public class T3_Insertar_JAVA
+{
+    public static void main(String[] args)
+    {
+        int[] numeros = {4,5,6,1,2,3,7,8,9};
+        
+        
+        for (int i = 0; i < numeros.length; i++) 
+        {
+                System.out.print("[" + numeros[i] + "] ");
+        }
+    }
+}
---
+++ T3_RecorridosArr/T3_RecorridosArr_JAVA.java
@@ -0,0 +1,14 @@
+package T3_RecorridosArr;
+public class T3_RecorridosArr_JAVA
+{
+    public static void main(String[] args)
+    {
+        int[] numeros = {4,5,6,1,2,3,7,8,9};
+        
+        
+        for (int i = 0; i < numeros.length; i++) 
+        {
+                System.out.print("[" + numeros[i] + "] ");
+        }
+    }
+}
