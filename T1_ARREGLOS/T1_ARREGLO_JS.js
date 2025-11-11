6cc186aa3d3c4777e1fbbd8e6bd9146de4f91401
MiltonColdy <miltonapodacar15@gmail.com>
2025-11-11 00:44:11 -0700

    Examen

--- T1_ARREGLOS/T1_ARREGLO_JS.js
+++
@@ -1,5 +0,0 @@
-// Se dimensiona el arreglo :D
-let arr = [6,7,8,9,10];
-// Se imprime el contenido del arreglo
-console.log("El arreglo contiene: "+arr);
-console.log("El quinto número es: "+arr[4]);
--- T1_ARREGLOS/T1_ARREGLO_PY.py
+++
@@ -1,6 +0,0 @@
-"Se declara el arreglo"
-arr = [6,7,8,9,10]
-"Se imprime el contenido del arreglo."
-print("El arreglo contiene: ", str(arr))
-"El str sirve para convertir un valor númerico a string."
-print("El segundo elemento del arreglo es: "+ str(arr[1]))
